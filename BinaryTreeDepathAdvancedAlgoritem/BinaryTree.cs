098734d316da496f158e573d530a64024d55e062
LundrimRexhepi <lundrim4@hotmail.com>
2020-12-13 23:36:41 +0100

    OK

---
+++ BinaryTreeDepathAdvancedAlgoritem/BinaryTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeDepathAdvancedAlgoritem
+{
+    class BinaryTree
+    {
+        public class Node
+        {
+            public int data;
+            public Node left, right;
+
+            public Node(int item)
+            {
+                data = item;
+                left = right = null;
+            }
+        }
+
+        public static class Tree
+        {
+            public static Node root;
+
+            public static int Llogaritethelsine(Node node)
+            {
+                if (node == null)
+                    return 0;
+                else
+                {
+                    int leftDepth = Llogaritethelsine(node.left);
+                    int rightDepth = Llogaritethelsine(node.right);
+
+                    if (leftDepth > rightDepth)
+                        return (leftDepth + 1);
+                    else
+                        return (rightDepth + 1);
+                }
+            }
+
+            public static Node Mbush(Node rrenja, int vlera)
+            {
+                if (rrenja == null)
+                    rrenja = new Node(vlera);
+                else if (vlera <= rrenja.data)
+                    rrenja.left = Mbush(rrenja.left, vlera);
+                else if (vlera > rrenja.data)
+                    rrenja.right = Mbush(rrenja.right, vlera);
+
+                return rrenja;
+            }
+        }
+    }
+}
---
+++ BinaryTreeDepathAdvancedAlgoritem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static BinaryTreeDepathAdvancedAlgoritem.BinaryTree;
+
+namespace BinaryTreeDepathAdvancedAlgoritem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ju lutem shenojeni me , anterat e vargut");
+            string vlerat = Console.ReadLine();
+            Node rrenja = null;
+            foreach (var item in vlerat.Split(','))
+            {
+                rrenja = Tree.Mbush(rrenja, int.Parse(item));
+            }
+
+            Console.WriteLine("Thellesia eshte : " +  Tree.Llogaritethelsine(rrenja));
+        }
+    }
+}
